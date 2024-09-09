e3ee9fc53c04820d2b44b90051372e99be97329e
mrunalbhonde <bhondemrunal0@gmail.com>
2024-09-09 14:26:45 +0530

    user routes updates

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,57 @@
 // can add constraints to schema for a elegant solution
+// const mongoose = require('mongoose');
+
+// mongoose.connect("mongodb://localhost:27017/paytm")
+
+// // Create a Schema for Users
+// const userSchema = new mongoose.Schema({
+//     username: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//         trim: true,
+//         lowercase: true,
+//         minLength: 3,
+//         maxLength: 30
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         minLength: 6
+//     },
+//     firstName: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         maxLength: 50
+//     },
+//     lastName: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         maxLength: 50
+//     }
+// });
+
+// const accountSchema = new mongoose.Schema({
+//     userId: {
+//         type: mongoose.Schema.Types.ObjectId, // Reference to User model
+//         ref: 'User',
+//         required: true
+//     },
+//     balance: {
+//         type: Number,
+//         required: true
+//     }
+// });
+
+// const Account = mongoose.model('Account', accountSchema);
+// const User = mongoose.model('User', userSchema);
+
+// module.exports = {
+// 	User,
+//   Account,
+// };
 import mongoose from "mongoose";
 
 mongoose.connect("mongodb+srv://admin:VbmCBFTByiVJGAUl@cluster0.rkvlplz.mongodb.net/paytm");
@@ -10,9 +63,23 @@ const userSchema = new mongoose.Schema({
     password: String,
 });
 
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+})
+
 const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema);
 
 module.exports = {
-    User
+    User,
+    Account,
 }
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -112,5 +112,30 @@ app.put("/", async (req, authMiddleware, res) => {
 
 })
 
+app.get("/bulk", async(req,res)=>{
+    const filter = req.query.filter;
+
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+})
+
 
 module.exports = router;
